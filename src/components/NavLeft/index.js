d1efc1d881033328ecc5f518fe89c91d45a7a884
chengyh2go <chengyh2go@gmail.com>
2021-10-19 22:56:57 +0800

    logo

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import MenuConfig from '../../config/menuConfig'
-import {Menu,Icon} from 'antd'
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import './index.less'
+
+const { SubMenu } = Menu; //等价于const SubMenu = Menu.SubMenu
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div>this is navLeft</div>
+            <div>
+                <div className="logo">
+                    <img src="/assets/logo.svg" alt="" />
+                    <h1>Test MS</h1>
+                </div>
+                <Menu theme="dark">
+                    <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="2">Option 3</Menu.Item>
+                        <Menu.Item key="2">Option 4</Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </div>
         )
     }
 
